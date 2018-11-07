42c44c208f083444e20b2722e1a713a8e603ca5e
DESKTOP-EL58LL7\ixtla <tstecker@student.cscc.edu>
2018-11-07 16:57:33 -0500

    added shell.bat to launch shell with some useful ints just type shell to begin using the pythons backed database shell

--- shell.bat
+++ shell.bat
@@ -1,2 +1,6 @@
+echo welcome to the pythons databas shell
+echo don't forget to call import statements on usefuld things like
+echo from products.models import Product, Brand, ProductInstance, Category
+echo from products.models import PaymentCard, Wallette, History
 py manage.py shell
 
