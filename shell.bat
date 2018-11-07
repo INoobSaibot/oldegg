e495a64f2f0998ebe2fd4458bec9e7a2ff73677a
DESKTOP-EL58LL7\ixtla <tstecker@student.cscc.edu>
2018-11-07 17:16:34 -0500

    added shell.bat to launch shell with some useful hints

--- shell.bat
+++ shell.bat
@@ -1,4 +1,5 @@
 echo welcome to the pythons databas shell
+
 echo don't forget to call import statements on usefuld things like
 echo from products.models import Product, Brand, ProductInstance, Category
 echo from products.models import PaymentCard, Wallette, History
